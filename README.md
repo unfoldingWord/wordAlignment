74ff802562b8004e8fe39ca192c6bdb715303938
mannycolon <colonmanuel7@gmail.com>
2017-08-30 15:24:49 -0400

    created blank word alignement tool

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# alignmentTool
-Language alignment tool
+# wordAlignment
+Word alignment tool
---
+++ index.js
@@ -0,0 +1,8 @@
+import Container from './src/Container';
+
+const NAMESPACE = 'wordAlignment';
+
+export default {
+  name: NAMESPACE,
+  container: Container
+};

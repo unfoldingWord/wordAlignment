c622b567a2995e9f9b91d8365131a41b6e754c2e
mannycolon <colonmanuel7@gmail.com>
2017-10-04 16:28:05 -0400

    made work bank area a drop zone to move back words and added actions that perform the work bank item moving

--- src/components/WordBankArea/index.js
+++ src/components/WordBankArea/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { DropTarget } from 'react-dnd';
+// constants
+import ItemTypes from '../ItemTypes';
 // components
 import WordBankItem from '../WordBankItem';
 
 const WordBankArea = ({
   contextIdReducer,
-  wordAlignmentReducer
+  wordAlignmentReducer,
+  connectDropTarget,
+  isOver
 }) => {
   if (contextIdReducer.contextId) {
     const { chapter, verse } = contextIdReducer.contextId.reference;
@@ -15,9 +20,11 @@ const WordBankArea = ({
       wordBank = wordAlignmentReducer.alignmentData[chapter][verse].wordBank;
     }
 
-    return (
+    return connectDropTarget(
       <div style={{ flex: 0.2, width: '100%', backgroundColor: '#DCDCDC', overflowY: 'auto', padding: '5px 8px 5px 5px' }}>
         {
+          isOver ? <div style={{ border: '3px dashed #44C6FF', height: '100%', width: '100%' }}></div>
+          : 
           wordBank.map((metadata, index) => (
             <WordBankItem
               key={index}
@@ -35,7 +42,26 @@ const WordBankArea = ({
 
 WordBankArea.propTypes = {
   contextIdReducer: PropTypes.object.isRequired,
-  wordAlignmentReducer: PropTypes.object.isRequired
+  wordAlignmentReducer: PropTypes.object.isRequired,
+  connectDropTarget: PropTypes.func.isRequired
 };
 
-export default WordBankArea;
+const wordBankAreaItemAction = {
+  drop(props, monitor) {
+    props.actions.moveBackToWordBank(monitor.getItem());
+  }
+};
+
+const collect = (connect, monitor) => {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver(),
+    canDrop: monitor.canDrop()
+  };
+};
+
+export default DropTarget(
+  ItemTypes.BOTTOM_WORD, // itemType
+  wordBankAreaItemAction,
+  collect
+)(WordBankArea);

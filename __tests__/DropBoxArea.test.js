1986ea089efd3931494d5d6f934ad503c4811135
Bruce McLean <mcleanb@photo-nomad.net>
2017-12-25 12:45:30 -0500

    DropBoxArea.test.js - added unit tests.

--- __tests__/DropBoxArea.test.js
+++ __tests__/DropBoxArea.test.js
@@ -1,13 +1,75 @@
 import React from 'react';
 import DropBoxArea from '../src/components/DropBoxArea';
-import renderer from 'react-test-renderer';
+import {shallow} from 'enzyme';
 
-test('DropBoxArea renders', () => {
-    const component = renderer.create(
-        <DropBoxArea />
+describe('DropBoxArea', () => {
+  let contextIdReducer ,wordAlignmentReducer, resourcesReducer;
+  const luke1 = require('./fixtures/luke/1.json');
+
+  beforeEach(() => {
+    contextIdReducer = {
+      "contextId": {
+        "groupId": "figs_metaphor",
+        "occurrence": 1,
+        "quote": "that he put before them",
+        "information": "Paul speaks about good deeds as if they were objects that God could place in front of people. AT: \"that God prepared for them to do\" (See: [[:en:ta:vol1:translate:figs_metaphor]]) \n",
+        "reference": {
+          "bookId": "luk",
+          "chapter": 1,
+          "verse": 1
+        },
+        "tool": "TranslationNotesChecker"
+      }
+    };
+    wordAlignmentReducer = {
+      alignmentData: {
+        "1": luke1
+      }
+    };
+    resourcesReducer = {
+      lexicons: {}
+    };
+  });
+
+  test('DropBoxArea renders Luke 1:1', () => {
+    const chapter = "1";
+    const verse = "1";
+    contextIdReducer.contextId.reference.chapter = chapter;
+    contextIdReducer.contextId.reference.verse = verse;
+    const expectedWords = wordAlignmentReducer.alignmentData[chapter][verse].alignments.length;
+    const enzymeWrapper = shallow(
+      <DropBoxArea
+        wordAlignmentReducer={wordAlignmentReducer}
+        contextIdReducer={contextIdReducer}
+        actions={{}}
+        resourcesReducer={resourcesReducer}
+      />
     );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
 
-    // TODO: exercise UI
+    const dropBoxArea = enzymeWrapper.find('div');
+    expect(dropBoxArea).toBeTruthy();
+    expect(dropBoxArea.nodes.length).toEqual(expectedWords + 1);
+  });
+
+  test('DropBoxArea renders undefined Luke 1:81 without crashing', () => {
+    const chapter = "1";
+    const verse = "81";
+    contextIdReducer.contextId.reference.chapter = chapter;
+    contextIdReducer.contextId.reference.verse = verse;
+    const expectedWords = 0;
+    const enzymeWrapper = shallow(
+      <DropBoxArea
+        wordAlignmentReducer={wordAlignmentReducer}
+        contextIdReducer={contextIdReducer}
+        actions={{}}
+        resourcesReducer={resourcesReducer}
+      />
+    );
+
+    const dropBoxArea = enzymeWrapper.find('div');
+    expect(dropBoxArea).toBeTruthy();
+    expect(dropBoxArea.nodes.length).toEqual(expectedWords + 1);
+  });
+
+  // TODO: exercise UI
 });

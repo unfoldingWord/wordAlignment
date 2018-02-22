33c4585249dd9fbda4f851224bc6430e4f08636c
Joel Lonbeck <joel@neutrinographics.com>
2018-02-21 16:00:00 -0800

    finished tool hoc

--- src/tctool/Tool.js
+++ src/tctool/Tool.js
@@ -9,71 +9,22 @@ import {getTranslate, getLocaleLoaded} from './state/reducers/index';
 
 export const connectTool = (toolId, localeDir) => {
   return (WrappedComponent) => {
-    class ToolComponent extends React.Component {
-
-      constructor(props) {
-        super(props);
-        this.state = {
-          broken: false,
-          error: null,
-          info: null
-        };
-      }
-
+    class ConnectedTool extends React.Component {
       render () {
-        const {broken, error, info} = this.state;
-        const Provider = this.Provider;
-
-        const isLocaleLoaded = getLocaleLoaded(this.store.getState());
-        if(!isLocaleLoaded) {
-          // TODO: we could display a loading screen while the locale loads
-          return null;
-        }
-
-        if(broken) {
-          // TODO: log the error to the core app state so it will be included in feedback logs.
-          // it would be best to pass a callback into this component for this purpose.
-          const translate = getTranslate(this.store.getState());
-          return <BrokenScreen title={translate('tool_broken')} error={error} info={info}/>;
-        } else {
-          return (
-            <Provider store={this.store}>
-              <WrappedComponent
-                {...this.props}
-              />
-            </Provider>
-          );
-        }
-      }
-
-      componentWillMount() {
         const {appLanguage} = this.props;
-        this.store = configureStore();
-        this.store.dispatch(loadLocalization(localeDir, appLanguage));
-        this.Provider = createProvider(toolId);
-        this.unsubscribe = this.store.subscribe(this.handleChange.bind(this));
-      }
-
-      componentWillUnmount() {
-        this.unsubscribe();
-      }
-
-      handleChange() {
-        this.forceUpdate();
-      }
-
-      componentDidCatch(error, info) {
-        this.setState({
-          broken: true,
-          error,
-          info
-        });
+        return (
+          <Tool localeDir={localeDir} toolId={toolId} appLanguage={appLanguage}>
+            <WrappedComponent
+              {...this.props}
+            />
+          </Tool>
+        );
       }
     }
-    ToolComponent.propTypes = {
+    ConnectedTool.propTypes = {
       appLanguage: PropTypes.string.isRequired
     };
-    return ToolComponent;
+    return ConnectedTool;
   };
 };
 
@@ -89,6 +40,7 @@ class Tool extends React.Component {
 
   constructor(props) {
     super(props);
+    this._isLocaleLoaded = this._isLocaleLoaded.bind(this);
     this.state = {
       broken: false,
       error: null,
@@ -97,10 +49,10 @@ class Tool extends React.Component {
   }
 
   componentWillMount() {
-    const {appLanguage, localeDir, storeKey} = this.props;
+    const {appLanguage, localeDir, toolId} = this.props;
     this.store = configureStore();
     this.store.dispatch(loadLocalization(localeDir, appLanguage));
-    this.Provider = createProvider(storeKey);
+    this.Provider = createProvider(toolId);
     this.unsubscribe = this.store.subscribe(this.handleChange.bind(this));
   }
 
@@ -127,34 +79,68 @@ class Tool extends React.Component {
     }
   }
 
+  /**
+   * Checks if the locale has finished loading
+   * @return {bool}
+   * @private
+   */
+  _isLocaleLoaded() {
+    return getLocaleLoaded(this.store.getState());
+  }
+
+  /**
+   * Returns the broken screen if one should be displayed
+   * @return {*}
+   * @private
+   */
+  _getBrokenScreen() {
+    const {broken, error, info} = this.state;
+    const translate = getTranslate(this.store.getState());
+    if(broken) {
+      // TODO: log the error to the core app state so it will be included in feedback logs.
+      // it would be best to pass a callback into this component for this purpose.
+      return <BrokenScreen title={translate('tool_broken')}
+                           error={error}
+                           info={info}/>;
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * Wraps the component with the provider
+   * @param {*} component - the child of this component
+   * @return {*}
+   * @private
+   */
+  _connectProvider(component) {
+    const Provider = this.Provider;
+    return (
+      <Provider store={this.store}>
+        {component}
+      </Provider>
+    );
+  }
+
   render() {
     const {children} = this.props;
-    const {broken, error, info} = this.state;
-    const Provider = this.Provider;
 
-    const isLocaleLoaded = getLocaleLoaded(this.store.getState());
-    if(!isLocaleLoaded) {
+    if(!this._isLocaleLoaded()) {
       // TODO: we could display a loading screen while the locale loads
       return null;
     }
 
-    if(broken) {
-      // TODO: log the error to the core app state so it will be included in feedback logs.
-      // it would be best to pass a callback into this component for this purpose.
-      const translate = getTranslate(this.store.getState());
-      return <BrokenScreen title={translate('tool_broken')} error={error} info={info}/>;
+    const brokenScreen = this._getBrokenScreen();
+    if(brokenScreen) {
+      return brokenScreen;
     } else {
-      return (
-        <Provider store={this.store}>
-          {children}
-        </Provider>
-      );
+      return this._connectProvider(children);
     }
   }
 }
 
 Tool.propTypes = {
-  storeKey: PropTypes.string.isRequired,
+  toolId: PropTypes.string.isRequired,
   children: PropTypes.any.isRequired,
   appLanguage: PropTypes.string.isRequired,
   localeDir: PropTypes.string.isRequired
--- src/tctool/index.js
+++ src/tctool/index.js
@@ -2,6 +2,8 @@ import Tool,  {connectTool} from './Tool';
 import { createConnect } from './state/store';
 import { getTranslate } from './state/reducers';
 
+export default Tool;
+
 exports.connectTool = connectTool;
 exports.createConnect = createConnect;
 exports.getTranslate = getTranslate;

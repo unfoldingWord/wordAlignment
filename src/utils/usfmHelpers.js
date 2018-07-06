27913f09bb7d40e5a5a3d1d3b566682faa95a6cd
mannycolon <colonmanuel7@gmail.com>
2018-07-05 20:19:57 -0400

    added fixed for footnotes

--- src/utils/usfmHelpers.js
+++ src/utils/usfmHelpers.js
@@ -1,6 +1,12 @@
+import usfmjs from 'usfm-js';
 
 export const removeUsfmMarkers = (targetVerseText) => {
+  const cleaned = usfmjs.removeMarker(
+    targetVerseText,
+    ['f', 'q(\\d)?', 's(\\d)?', 'p(\\d)?']
+  ); // remove these markers, 'f' is predefined
+
   const regString = '\\\\\\w[0-9]*';
   const regex = new RegExp(regString, 'g');
-  return targetVerseText.replace(regex, '');
+  return cleaned.replace(regex, '');
 };

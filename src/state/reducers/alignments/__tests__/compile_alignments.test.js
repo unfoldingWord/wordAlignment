5c1592808416b02e5480423e19696ec1992df546
Bruce McLean <mcleanb@photo-nomad.net>
2018-08-02 10:23:30 -0400

    Adding a test case for handling suggested splits.

--- src/state/reducers/alignments/__tests__/compile_alignments.test.js
+++ src/state/reducers/alignments/__tests__/compile_alignments.test.js
@@ -284,7 +284,7 @@ describe('compiles alignments', () => {
           "3": [3], 
           "4": [4], 
           "5": [4, 4], // not sure why there are two entries of the same value here, or why there should ever be more than one
-          "6": [5]} // TODO: the current algorithm is putting a [6] here which is out of range and causes a later crash when you align the word back in
+          "6": [5]} // TODO: the current algorithm is putting a [6] here which is out of range and causes a later crash when you align a word back in
       };
       const renderedAlignments_= [ // TODO: for reference only: expected output after call to verse
         {"sourceNgram": [0], "targetNgram": [1], "alignments": [0]},
@@ -293,7 +293,7 @@ describe('compiles alignments', () => {
         {"sourceNgram": [3], "targetNgram": [3], "alignments": [3]},
         {"sourceNgram": [4], "targetNgram": [], "suggestedTargetTokens": [], "alignments": [4], "suggestion": 4},
         {"sourceNgram": [5], "targetNgram": [], "suggestedTargetTokens": [], "alignments": [4, 4], "suggestion": 5},
-        {"sourceNgram": [6, 7], "targetNgram": [6, 7], "alignments": [5]}
+        {"sourceNgram": [6, 7], "targetNgram": [6, 7], "alignments": [5]} // TODO: the current algorithm is putting a [6] for alignment which is out of range and causes a later crash when you align a word back in
       ];
       const result = compile(rendered, alignments);
       expect(result).toEqual(expected);

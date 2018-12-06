1ad14ea3e7e14fd50feff3f3d1a5c1e25c4de693
Bruce McLean <mcleanb@photo-nomad.net>
2018-12-06 13:48:10 -0500

    fix problem when first book opened after install is OT

--- src/utils/panesSettingsHelper.js
+++ src/utils/panesSettingsHelper.js
@@ -1,18 +1,25 @@
 
 export const sortPanesSettings = (currentPanes, setToolSettings, bibles) => {
   // filter out targetLanguage and OL
-  let isOT = false;
+  let olBible = null;
   let panes = currentPanes.filter((pane) => {
     let filterOut = (pane.languageId === 'targetLanguage'); // strip target language which will be placed first
     if (!filterOut) {
       const isOL = ['bhp','ugnt','uhb'].includes(pane.bibleId);
       if (isOL) {  // strip OL which will be place second
+        olBible = pane.bibleId;
         filterOut = isOL;
-        isOT = (pane.bibleId === 'uhb');
       }
     }
     return !filterOut;
   });
+
+  let isOT = false;
+  if (olBible) {
+    isOT = (olBible === 'uhb');
+  } else { // if no OL given, check in bibles
+    isOT = bibles && bibles.originalLanguage && bibles.originalLanguage.uhb;
+  }
 
   // filter out bibles that are not found in the resources reducer
   panes = panes.filter((paneSetting) => {

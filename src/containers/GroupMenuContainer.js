2b0d883fecc6652f2b156102a7e5e83f60141496
Joel Lonbeck <joel@neutrinographics.com>
2019-01-11 16:03:21 -0800

    cleaned out old menu code

--- src/containers/GroupMenuContainer.js
+++ src/containers/GroupMenuContainer.js
@@ -12,76 +12,13 @@ import EditIcon from '@material-ui/icons/Edit';
 import {getChecks} from '../state/reducers';
 
 class GroupMenuContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this._handleIsVerseValid = this._handleIsVerseValid.bind(this);
-    this._handleGetSelections = this._handleGetSelections.bind(this);
-    this._handleGetGroupProgress = this._handleGetGroupProgress.bind(this);
-  }
-
-  getGroupProgress(groupIndex, groupsData, isVerseFinished) {
-    let groupId = groupIndex.id;
-    let totalChecks = groupsData[groupId].length;
-    const doneChecks = groupsData[groupId].filter(groupData => {
-      let verseFinished = false;
-      const reference = groupData && groupData.contextId &&
-      groupData.contextId.reference ? groupData.contextId.reference : null;
-      if (reference) {
-        verseFinished = isVerseFinished(reference.chapter, reference.verse);
-      }
-      return verseFinished && !groupData.reminders;
-    }).length;
-
-    return doneChecks / totalChecks;
-  }
-
-  /**
-   * Handles check to determine if a verse is valid
-   * @param {number} chapter
-   * @param {number} verse
-   * @return {boolean}
-   * @private
-   */
-  _handleIsVerseValid(chapter, verse) {
-    const {toolApi} = this.props;
-    return !toolApi.getIsVerseInvalid(chapter, verse);
-  }
-
-  /**
-   * Handles retrieving selections
-   * @param contextId
-   * @private
-   */
-  _handleGetSelections(contextId) {
-    const {
-      tc: {
-        actions: {getSelectionsFromContextId}
-      },
-      projectSaveLocation
-    } = this.props;
-    return getSelectionsFromContextId(contextId, projectSaveLocation);
-  }
-
-  /**
-   * Handles fetching the group progress
-   * @param groupIndex
-   * @param groupsData
-   * @return {*}
-   * @private
-   */
-  _handleGetGroupProgress(groupIndex, groupsData) {
-    const {
-      isVerseFinished
-    } = this.props;
-    return this.getGroupProgress(groupIndex, groupsData, isVerseFinished);
-  }
 
   /**
    * Handles click events from the menu
    * @param {object} contextId - the menu item's context id
    */
   handleClick = contextId => {
-    const {actions: {changeCurrentContextId}} = this.props;
+    const {tc: {actions: {changeCurrentContextId}}} = this.props;
     changeCurrentContextId(contextId);
   };
 
@@ -104,22 +41,18 @@ class GroupMenuContainer extends React.Component {
       ...item,
       title: `${bookName} ${chapter}:${verse}`,
       completed: toolApi.getIsVerseFinished(chapter, verse), // TODO: I could read from state if I load these into the reducer at startup.
-      invalid: toolApi.getIsVerseInvalid(chapter, verse),
+      invalid: toolApi.getIsVerseInvalid(chapter, verse)
     };
   };
 
   render() {
     const {
       translate,
-      actions,
-      isVerseFinished,
-      groupsDataReducer,
-      groupsIndexReducer,
-      groupMenuReducer,
-      toolsReducer,
-      contextId,
-      manifest,
-      projectSaveLocation
+      tc: {
+        contextId,
+        groupsDataReducer: {groupsData},
+        groupsIndexReducer: {groupsIndex}
+      }
     } = this.props;
 
     const filters = [
@@ -183,8 +116,8 @@ class GroupMenuContainer extends React.Component {
     ];
 
     const entries = generateMenuData(
-      groupsIndexReducer.groupsIndex,
-      groupsDataReducer.groupsData,
+      groupsIndex,
+      groupsData,
       'completed',
       this.onProcessItem
     );
@@ -198,19 +131,6 @@ class GroupMenuContainer extends React.Component {
         emptyNotice="No results found"
         title="Menu"
         onItemClick={this.handleClick}
-
-        // translate={translate}
-        // getSelections={this._handleGetSelections}
-        // getGroupProgress={this._handleGetGroupProgress}
-        // isVerseFinished={isVerseFinished}
-        // isVerseValid={this._handleIsVerseValid}
-        // groupsDataReducer={groupsDataReducer}
-        // groupsIndexReducer={groupsIndexReducer}
-        // groupMenuReducer={groupMenuReducer}
-        // toolsReducer={toolsReducer}
-        // contextIdReducer={{contextId}}
-        // projectDetailsReducer={{manifest, projectSaveLocation}}
-        // actions={actions}
       />
     );
   }
@@ -219,37 +139,12 @@ class GroupMenuContainer extends React.Component {
 GroupMenuContainer.propTypes = {
   tc: PropTypes.object.isRequired,
   toolApi: PropTypes.instanceOf(Api),
-  translate: PropTypes.func.isRequired,
-
-  // mapped from props
-  actions: PropTypes.object.isRequired,
-  isVerseFinished: PropTypes.func.isRequired,
-  groupsDataReducer: PropTypes.object.isRequired,
-  groupsIndexReducer: PropTypes.object.isRequired,
-  groupMenuReducer: PropTypes.object.isRequired,
-  toolsReducer: PropTypes.object.isRequired,
-  contextId: PropTypes.object.isRequired,
-  manifest: PropTypes.object.isRequired,
-  projectSaveLocation: PropTypes.string.isRequired
+  translate: PropTypes.func.isRequired
 };
 
-const mapStateToProps = (state, props) => {
-  const {tc, translate, toolApi} = props;
-
-  return {
-    completedChecks: getChecks(state, "completed"),
-    toolsReducer: {currentToolName: tc.selectedToolName},
-    groupsDataReducer: tc.groupsDataReducer,
-    groupsIndexReducer: tc.groupsIndexReducer,
-    groupMenuReducer: tc.groupMenuReducer,
-    translate,
-    actions: tc.actions,
-    isVerseFinished: toolApi.getIsVerseFinished,
-    isVerseValid: toolApi.getIsVerseInvalid,
-    contextId: tc.contextId,
-    manifest: tc.projectDetailsReducer.manifest,
-    projectSaveLocation: tc.projectDetailsReducer.projectSaveLocation
-  };
-};
+const mapStateToProps = (state) => ({
+  // TRICKY: bind these to listen to state change events
+  completedChecks: getChecks(state, 'completed')
+});
 
 export default connect(mapStateToProps)(GroupMenuContainer);

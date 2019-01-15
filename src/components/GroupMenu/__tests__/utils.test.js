7df6b8bc4171cbc987f9c9d398d406891bbb4132
Joel Lonbeck <joel@neutrinographics.com>
2019-01-14 18:06:29 -0800

    added tests for menu data generator

---
+++ src/components/GroupMenu/__tests__/utils.test.js
@@ -0,0 +1,154 @@
+import {generateMenuData} from '../utils';
+
+describe('generate menu data', () => {
+  it('has empty input data', () => {
+    const groupIndex = [];
+    const groupData = {};
+    const progressKey = 'complete';
+    const onProcessItem = null;
+    const menuData = generateMenuData(groupIndex, groupData, progressKey,
+      onProcessItem);
+    expect(menuData).toEqual([]);
+  });
+
+  it('has a custom item processor', () => {
+    const groupIndex = [
+      {
+        id: 'abraham',
+        name: 'Abraham'
+      }, {
+        id: 'adoption',
+        name: 'adoption, adopt, adopted'
+      }];
+    const groupData = {
+      adoption: [
+        {
+          contextId: {
+            reference: {
+              bookId: 'gal',
+              chapter: 4,
+              verse: 5
+            },
+            groupId: 'adoption',
+            occurrence: 1
+          }
+        }]
+    };
+    const progressKey = 'completed';
+    const onProcessItem = item => ({
+      ...item,
+      customField: 'hello world',
+      title: item.contextId.reference.bookId
+    });
+    const expected = [
+      {
+        children: [
+          {
+            contextId: {
+              groupId: 'adoption',
+              occurrence: 1,
+              reference: {'bookId': 'gal', 'chapter': 4, 'verse': 5}
+            },
+            title: 'gal',
+            customField: 'hello world'
+          }],
+        id: 'adoption',
+        progress: 0,
+        title: 'adoption, adopt, adopted'
+      }];
+    const menuData = generateMenuData(groupIndex, groupData, progressKey,
+      onProcessItem);
+    expect(menuData).toEqual(expected);
+  });
+
+  it('has input data', () => {
+    const groupIndex = [
+      {
+        id: 'abraham',
+        name: 'Abraham'
+      }, {
+        id: 'adoption',
+        name: 'adoption, adopt, adopted'
+      }];
+    const groupData = {
+      abraham: [
+        {
+          contextId: {
+            reference: {
+              bookId: 'gal',
+              chapter: 3,
+              verse: 6
+            },
+            groupId: 'abraham',
+            occurrence: 1
+          },
+          completed: true
+        }, {
+          contextId: {
+            reference: {
+              bookId: 'gal',
+              chapter: 3,
+              verse: 7
+            },
+            groupId: 'abraham',
+            occurrence: 1
+          }
+        }],
+      adoption: [
+        {
+          contextId: {
+            reference: {
+              bookId: 'gal',
+              chapter: 4,
+              verse: 5
+            },
+            groupId: 'adoption',
+            occurrence: 1
+          }
+        }]
+    };
+    const progressKey = 'completed';
+    const onProcessItem = null;
+    const expected = [
+      {
+        children: [
+          {
+            contextId: {
+              groupId: 'abraham',
+              occurrence: 1,
+              reference: {'bookId': 'gal', 'chapter': 3, 'verse': 6}
+            },
+            completed: true,
+            title: 'gal 3:6'
+          },
+          {
+            contextId: {
+              groupId: 'abraham',
+              occurrence: 1,
+              reference: {'bookId': 'gal', 'chapter': 3, 'verse': 7}
+            },
+            title: 'gal 3:7'
+          }],
+        id: 'abraham',
+        progress: 50,
+        title: 'Abraham'
+      },
+      {
+        children: [
+          {
+            contextId: {
+              groupId: 'adoption',
+              occurrence: 1,
+              reference: {'bookId': 'gal', 'chapter': 4, 'verse': 5}
+            },
+            title: 'gal 4:5'
+          }],
+        id: 'adoption',
+        progress: 0,
+        title: 'adoption, adopt, adopted'
+      }];
+    const menuData = generateMenuData(groupIndex, groupData, progressKey,
+      onProcessItem);
+    expect(menuData).toEqual(expected);
+  });
+});
--- src/state/reducers/checks/index.js
+++ src/state/reducers/checks/index.js
@@ -1,6 +1,21 @@
 import {RECORD_CHECK} from '../../actions/actionTypes';
 import check, * as fromCheck from './check';
 
+// TODO: the "checks" reducer is the beginning concept of a generic checks handler that could be used for all tools.
+// The basic idea is that the tc-tool could provide utility actions and selectors for managing state within a tool.
+// We'll need to figure out some more complicated things like how to represent multiple checks for a single verse.
+// For example tW could have checks for multiple instances of a word or phrase within the same verse.
+// @example
+// const {tool: {
+//    recordCheck, // this is an action
+//    getCheck // this is a selector
+//  }
+// } = this.props;
+// recordCheck("invalidated", "some data"); // the new check record will be stored in redux and return immediately (fast). Check state will be written to disk asynchronously without blocking the ui.
+// const verseChecks = getChecks("invalidated", 1, 1); // returns the latest checks for the verse from redux (fast)
+//
+//
+
 /**
  * Reduces checks for the tool
  * @param state
@@ -29,7 +44,7 @@ export default checks;
  * @param {number} verse
  */
 export const getVerseCheck = (state, check, chapter, verse) => {
-  if(check in state) {
+  if (check in state) {
     return fromCheck.getVerseCheck(state[check], chapter, verse);
   } else {
     return null;
@@ -42,7 +57,7 @@ export const getVerseCheck = (state, check, chapter, verse) => {
  * @param {string} check
  */
 export const getChecks = (state, check) => {
-  if(check in state) {
+  if (check in state) {
     return state[check];
   } else {
     return null;

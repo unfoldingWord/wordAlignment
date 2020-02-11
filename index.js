a1d542cc737758b8cba3eee4ba12e8fd9ba1ce7d
Bruce McLean <mcleanb@photo-nomad.net>
2020-02-11 08:16:22 -0500

    webpack support

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import reducer from './src/state/reducers';
 import Api from './src/Api';
 
 export default connectTool('wordAlignment', {
-  localeDir: path.join(__dirname, 'src/locale'),
+  localeDir: path.join('./src/tC_apps/wordAlignment/src/locale'),
   reducer: reducer,
   api: new Api(),
 })(Container);

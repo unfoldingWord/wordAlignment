1f1eb6e7ad90302cc3255f4d9154132b3907a221
Bruce McLean <mcleanb@photo-nomad.net>
2020-03-21 06:37:56 -0400

    cleanup

--- src/components/__tests__/Container-Render.test.js
+++ src/components/__tests__/Container-Render.test.js
@@ -9,12 +9,6 @@ import Api from '../../Api';
 
 jest.mock('../../state/reducers');
 
-const { app, remote } = require('electron');
-
-console.log("app" + !!app);
-console.log("remote" + !!remote);
-
-
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 

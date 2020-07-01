ac6f0dac8bf3486f613aae6960dfea9b3b019684
mannycolon <colonmanuel7@gmail.com>
2020-07-01 10:07:45 -0400

    Eslint fixes

--- src/utils/panesSettingsHelper.js
+++ src/utils/panesSettingsHelper.js
@@ -4,8 +4,10 @@ export const sortPanesSettings = (currentPanes, setToolSettings, bibles) => {
   let olBible = null;
   let panes = currentPanes.filter((pane) => {
     let filterOut = (pane.languageId === 'targetLanguage'); // strip target language which will be placed first
+
     if (!filterOut) {
       const isOL = ['bhp','ugnt','uhb'].includes(pane.bibleId);
+
       if (isOL) {// strip OL which will be place second
         olBible = pane.bibleId;
         filterOut = isOL;
@@ -15,6 +17,7 @@ export const sortPanesSettings = (currentPanes, setToolSettings, bibles) => {
   });
 
   let isOT = false;
+
   if (olBible) {
     isOT = (olBible === 'uhb');
   } else { // if no OL given, check in bibles
@@ -22,29 +25,28 @@ export const sortPanesSettings = (currentPanes, setToolSettings, bibles) => {
   }
 
   // filter out bibles that are not found in the resources reducer
-  panes = panes.filter((paneSetting) => {
-    return isPaneSettingFoundInBibles(bibles, paneSetting);
-  });
+  panes = panes.filter((paneSetting) => isPaneSettingFoundInBibles(bibles, paneSetting));
 
   // set the ScripturePane to display targetLanguage and bhp for the word alignment tool from left to right.
   let desiredPanes = [
     {
       languageId: 'targetLanguage',
-      bibleId: 'targetBible'
+      bibleId: 'targetBible',
     },
     {
       languageId: 'originalLanguage',
-      bibleId: isOT ? 'uhb' : 'ugnt'
-    }
+      bibleId: isOT ? 'uhb' : 'ugnt',
+    },
   ];
 
   desiredPanes = desiredPanes.map(desiredPane => {
     const foundPane = currentPanes.find(pane =>
       pane.languageId === desiredPane.languageId && pane.bibleId === desiredPane.bibleId);
+
     if (foundPane) {
       return {
         ...foundPane,
-        ...desiredPane
+        ...desiredPane,
       };
     } else {
       return desiredPane;
@@ -59,6 +61,4 @@ export const sortPanesSettings = (currentPanes, setToolSettings, bibles) => {
   setToolSettings('ScripturePane', 'currentPaneSettings', desiredPanes);
 };
 
-const isPaneSettingFoundInBibles = (bibles, paneSetting) => {
-  return bibles[paneSetting.languageId] && bibles[paneSetting.languageId][paneSetting.bibleId] ? true : false;
-};
+const isPaneSettingFoundInBibles = (bibles, paneSetting) => bibles[paneSetting.languageId] && bibles[paneSetting.languageId][paneSetting.bibleId] ? true : false;

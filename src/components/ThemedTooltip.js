d7e5bd92470d692fa572f8919c0760a5f77286d5
Bruce McLean <mcleanb@photo-nomad.net>
2020-07-16 15:55:36 -0400

    cleanup, update snapshots

--- src/components/ThemedTooltip.js
+++ src/components/ThemedTooltip.js
@@ -3,53 +3,6 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Tooltip from '@material-ui/core/Tooltip';
 
-function arrowGenerator(color) {
-  return {
-    '&[x-placement*="bottom"] $arrow': {
-      top: 8,
-      left: 0,
-      marginTop: '-0.95em',
-      width: '3em',
-      height: '1em',
-      '&::before': {
-        borderWidth: '0 1em 1em 1em',
-        borderColor: `transparent transparent ${color} transparent`,
-      },
-    },
-    '&[x-placement*="top"] $arrow': {
-      bottom: 0,
-      left: 0,
-      marginBottom: '-0.95em',
-      width: '3em',
-      height: '1em',
-      '&::before': {
-        borderWidth: '1em 1em 0 1em',
-        borderColor: `${color} transparent transparent transparent`,
-      },
-    },
-    '&[x-placement*="right"] $arrow': {
-      left: 0,
-      marginLeft: '-0.95em',
-      height: '3em',
-      width: '1em',
-      '&::before': {
-        borderWidth: '1em 1em 1em 0',
-        borderColor: `transparent ${color} transparent transparent`,
-      },
-    },
-    '&[x-placement*="left"] $arrow': {
-      right: 0,
-      marginRight: '-0.95em',
-      height: '3em',
-      width: '1em',
-      '&::before': {
-        borderWidth: '1em 0 1em 1em',
-        borderColor: `transparent transparent transparent ${color}`,
-      },
-    },
-  };
-}
-
 const styles = theme => ({
   arrow: {
     'fontSize': 16,
@@ -60,21 +13,6 @@ const styles = theme => ({
       boxSizing: 'border-box',
     },
   },
-  // arrow: {
-  //   position: 'absolute',
-  //   fontSize: 6,
-  //   width: '3em',
-  //   height: '3em',
-  //   '&::before': {
-  //     content: '""',
-  //     margin: 'auto',
-  //     display: 'block',
-  //     width: 0,
-  //     height: 0,
-  //     borderStyle: 'solid',
-  //   },
-  // },
-  bootstrapPopper: arrowGenerator(theme.palette.common.black),
   bootstrapTooltip: {
     backgroundColor: theme.palette.common.black,
     fontSize: "inherit",
@@ -82,18 +20,6 @@ const styles = theme => ({
     maxWidth: 375,
     wordBreak: "break-all"
   },
-  bootstrapPlacementLeft: {
-    margin: '0 8px',
-  },
-  bootstrapPlacementRight: {
-    margin: '0 8px',
-  },
-  bootstrapPlacementTop: {
-    margin: '8px 0',
-  },
-  bootstrapPlacementBottom: {
-    margin: '8px 0',
-  }
 });
 
 /**
@@ -123,7 +49,6 @@ class ThemedTooltip extends React.Component {
 
   render() {
     const { message , children, classes, disabled, targetLanguageFontClassName, fontScale} = this.props;
-    const { arrowRef } = this.state;
 
     return (
       <Tooltip
@@ -134,22 +59,15 @@ class ThemedTooltip extends React.Component {
         enterDelay={400}
         leaveDelay={200}
         title={
-          // <React.Fragment>
           <span style={{fontSize: `${fontScale}%`}}>
             <span className={targetLanguageFontClassName}>
               {message}
             </span>
           </span>
-        //     <span className={classes.arrow} ref={this.handleArrowRef} />
-        //   </React.Fragment>
         }
         classes={{
           tooltip: classes.bootstrapTooltip,
           arrow: classes.arrow,
-          tooltipPlacementLeft: classes.bootstrapPlacementLeft,
-          tooltipPlacementRight: classes.bootstrapPlacementRight,
-          tooltipPlacementTop: classes.bootstrapPlacementTop,
-          tooltipPlacementBottom: classes.bootstrapPlacementBottom,
         }}
       >
         {children}

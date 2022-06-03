939e874806179f5aabe8becfb83ec093cab17c63
blm <mcleanb@photo-nomad.net>
2022-06-03 11:25:54 -0400

    add verse span support to getAlignedTextFromBible

--- src/utils/gatewayLanguageHelpers.js
+++ src/utils/gatewayLanguageHelpers.js
@@ -17,16 +17,57 @@ export function bibleIdSort(a, b) {
 }
 
 /**
+ * get verse range from span
+ * @param {string} verseSpan
+ * @return {{high: number, low: number}}
+ */
+export function getVerseSpanRange(verseSpan) {
+  let [low, high] = verseSpan.split('-');
+  low = parseInt(low);
+  high = parseInt(high);
+  return { low, high };
+}
+
+/**
+ * test if verse is valid verse number or verse span string
+ * @param {string|number} verse
+ * @return {boolean}
+ */
+export function isVerseSpan(verse) {
+  const isSpan = (typeof verse === 'string') && verse.includes('-');
+  return isSpan;
+}
+
+/**
  * Gets the aligned GL text from the given bible
  * @param {object} contextId
  * @param {object} bible
  * @returns {string}
  */
 export function getAlignedTextFromBible(contextId, bible) {
-  if (bible && contextId && contextId.reference &&
-    bible[contextId.reference.chapter] && bible[contextId.reference.chapter][contextId.reference.verse] &&
-    bible[contextId.reference.chapter][contextId.reference.verse].verseObjects) {
-    const verseObjects = bible[contextId.reference.chapter][contextId.reference.verse].verseObjects;
+  if (bible && contextId?.reference) {
+    const chapterData = bible[contextId.reference.chapter];
+    const verseRef = contextId.reference.verse;
+    const verseData = chapterData?.[verseRef];
+    let verseObjects = null;
+
+    if (verseData) { // if we found verse
+      verseObjects = verseData.verseObjects;
+    } else if (isVerseSpan(verseRef)) { // if we didn't find verse, check if verse span
+      verseObjects = [];
+      // iterate through all verses in span
+      const { low, high } = getVerseSpanRange(verseRef);
+
+      for (let i = low; i <= high; i++) {
+        const verseObjects_ = chapterData?.[i]?.verseObjects;
+
+        if (!verseObjects_) { // if verse missing, abort
+          verseObjects = null;
+          break;
+        }
+        verseObjects = verseObjects.concat(verseObjects_);
+      }
+    }
     return getAlignedText(verseObjects, contextId.quote, contextId.occurrence);
   }
 }

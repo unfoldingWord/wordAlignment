9653bd942554f1536df4239b5654a790c38aa769
PhotoNomad0 <bruce.mclean@unfoldingword.org>
2024-12-05 15:45:49 -0500

    Fix to get updated hasRenderedSuggestions property

--- src/components/Container.js
+++ src/components/Container.js
@@ -202,6 +202,7 @@ export class Container extends Component {
     this.updatePredictions = this.updatePredictions.bind(this);
     this.runMAP = this.runMAP.bind(this);
     this.initMAP = this.initMAP.bind(this);
+    this.getHasRenderedSuggestions = this.getHasRenderedSuggestions.bind(this)
     this.handleAlignTargetToken = this.handleAlignTargetToken.bind(this);
     this.handleUnalignTargetToken = this.handleUnalignTargetToken.bind(this);
     this.handleAlignPrimaryToken = this.handleAlignPrimaryToken.bind(this);
@@ -225,6 +226,7 @@ export class Container extends Component {
     this.handleVerseEditSubmit = this.handleVerseEditSubmit.bind(this);
     this.handleCommentClick = this.handleCommentClick.bind(this);
     this.handleCommentClose = this.handleCommentClose.bind(this);
+    this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
     this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
     this.state = {
       loading: false,
@@ -572,6 +574,13 @@ export class Container extends Component {
     this.handleResetWordList();
   }
 
+  getHasRenderedSuggestions() {
+    const {
+      hasRenderedSuggestions
+    } = this.props;
+    return !!hasRenderedSuggestions
+  }
+
   handleClearAlignments() {
     const {
       clearAllAlignments,
@@ -784,7 +793,7 @@ export class Container extends Component {
       },
       ACCEPT: () => {
         // console.log('A - Accept action triggered')
-        if (hasRenderedSuggestions) {
+        if (this.getHasRenderedSuggestions()) {
           this.handleAcceptSuggestions()
         } else {
           // console.log('No suggestions')
@@ -792,7 +801,7 @@ export class Container extends Component {
       },
       REJECT: () => {
         // console.log('J - Reject action triggered')
-        if (hasRenderedSuggestions) {
+        if (this.getHasRenderedSuggestions()) {
           this.handleRejectSuggestions()
         } else {
           // console.log('No suggestions')
